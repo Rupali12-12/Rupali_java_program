0664bdba9a6b0a9c6cb94466dee6c564d1b6d150
rupali <rupsja1813@gmail.cocm>
2023-03-31 08:18:50 +0530

    Addition prog

---
+++ src/main/java/basics/Addition.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10,b=12,c;
+		
+	    c=a+b;
+		System.out.println("print addition: " +c);
+
+	}
+
+}
